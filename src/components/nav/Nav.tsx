b5cb568c914ffdc55e421a5dcbe453e19cc50d8c
Kittoff <j'aimegithub>
2023-02-23 12:17:08 +0100

    fixed typo

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./Nav.css";
+import "./nav.css";
 import { GiJapan } from "react-icons/gi";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
